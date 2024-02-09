e16af34b1131fa7fec9dfeb1222edfcae3d55223
maheshnehere <maheshnehere93@gmail.com>
2024-02-09 22:09:11 +0530

    if else for ten units number

--- src/CFQ/Problem1.java
+++ src/CFQ/Problem1.java
@@ -3,17 +3,29 @@ package CFQ;
 //Read a single Digit Number and write in word
 
 
+import java.util.Scanner;
+
 public class Problem1 {
     public static void main(String[] args) {
-        int num =5;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int num = sc.nextInt();
 
-        if (num < 10)
-        {
-            System.out.println(num+": is a single digit number");
+        if (num >= 0 && num <= 9) {
+            System.out.println(num + ": is a single digit number");
         }
+            else if (num >= 10 && num <= 100) {
+            System.out.println(num + ":is a double digit number");
+        }
+                else if ( num > 100 && num < 1000)
+                {
+                    System.out.println(num+":is a triple digit number");
+                }
+
         else {
-            System.out.println(num+": is not a single digit number");
+            System.out.println("Invalid number");
         }
+
 
 
     }
