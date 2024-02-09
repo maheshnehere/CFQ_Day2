26f146a44e6aea28c2aefb55fddd2fda604e7136
maheshnehere <maheshnehere93@gmail.com>
2024-02-09 20:31:55 +0530

    if else statement used

--- src/CFQ/Problem1.java
+++ src/CFQ/Problem1.java
@@ -5,6 +5,16 @@ package CFQ;
 
 public class Problem1 {
     public static void main(String[] args) {
+        int num =5;
+
+        if (num < 10)
+        {
+            System.out.println(num+": is a single digit number");
+        }
+        else {
+            System.out.println(num+": is not a single digit number");
+        }
+
 
     }
 }
