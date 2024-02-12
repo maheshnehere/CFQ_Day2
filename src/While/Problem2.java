5f10e491a05d2df057a1f281b31def1c12fe5d42
maheshnehere <maheshnehere93@gmail.com>
2024-02-12 11:54:02 +0530

    reverse number

---
+++ src/While/Problem2.java
@@ -0,0 +1,21 @@
+package While;
+
+import java.util.Scanner;
+
+public class Problem2 {
+
+
+    public static void main(String[] args) {
+            int reverse = 0;
+            int number =1234;
+
+        while (number != 0) {
+            int remainder = number % 10;
+            reverse = reverse * 10 + remainder;
+            number = number / 10;
+        }
+
+        System.out.println("The reverse of the given number is: " + reverse);
+    }
+
+}
